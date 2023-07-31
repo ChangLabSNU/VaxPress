dab12f3c5052687b69daf5e8bef36c305d91ccf6
Hyeshik Chang <hyeshik@snu.ac.kr>
2023-07-31 16:53:15 +0900

    Convert to argparse from click.

--- vaxpress/__main__.py
+++ vaxpress/__main__.py
@@ -25,54 +25,67 @@
 
 from .evolution_chamber import CDSEvolutionChamber, ScoringOptions, IterationOptions
 from Bio import SeqIO
-import click
+import argparse
 
-@click.command()
-@click.option('-o', '--output', required=True, help='output file')
-@click.option('--seed', type=int, default=922, help='random seed')
-@click.option('--processes', default=5, help='number of processes to use')
-@click.option('--quiet', is_flag=True, help='do not print progress')
-@click.option('--print-top', default=10, help='print top and bottom N sequences')
-@click.option('--codon-table', default='standard', help='codon table')
-@click.option('--random-initialization', is_flag=True, help='randomize all codons at the beginning')
-# Iteration parameters
-@click.option('--iterations', default=10, help='number of iterations')
-@click.option('--offsprings', default=4, help='number of offsprings per iteration')
-@click.option('--survivors', default=2, help='number of survivors per iteration')
-@click.option('--initial-mutation-rate', default=0.1, help='initial mutation rate')
-@click.option('--winddown-trigger', default=15, help='number of iterations with the same best score to trigger mutation stabilization')
-@click.option('--winddown-rate', default=0.9, help='mutation rate multiplier when mutation stabilization is triggered')
-# iCodon parameters
-@click.option('--icodon-species', default='human', help='iCodon RNA stability optimizer: species')
-@click.option('--icodon-weight', default=1.0, help='scoring weight for iCodon predicted stability')
-# U count parameters
-@click.option('--ucount-weight', default=3.0, help='scoring weight for U count minimizer')
-# GC ratio parameters
-@click.option('--gc-window-size', default=50, help='size of window for GC content calculation')
-@click.option('--gc-stride', default=5, help='size of stride for GC content calculation')
-@click.option('--gc-weight', default=3, help='GC penalty score')
-# Secondary structure parameters
-@click.option('--folding-off', is_flag=True, help='disable secondary structure folding')
-@click.option('--folding-engine', default='vienna', help='RNA folding engine')
-@click.option('--folding-mfe-weight', default=1.0, help='scoring weight for MFE')
-@click.option('--folding-start-structure-width', default=15, help='width in nt of unfolded region near the start codon')
-@click.option('--folding-start-structure-weight', default=1, help='penalty weight for folded start codon')
-# Tandem repeats parameters
-@click.option('--repeats-min-repeats', default=2, help='minimum number of repeats')
-@click.option('--repeats-repeat-length', default=10, help='minimum length of repeats')
-@click.option('--repeats-weight', default=1.0, help='scoring weight for tandem repeats')
-# Arguments
-@click.argument('fasta', required=True)
-def run_vaxpress(output, seed, processes, quiet, print_top, codon_table, random_initialization,
-                 iterations, offsprings, survivors, initial_mutation_rate, winddown_trigger,
-                 winddown_rate, icodon_species, icodon_weight, ucount_weight,
-                 gc_window_size, gc_stride, gc_weight,
-                 folding_off, folding_engine, folding_mfe_weight,
-                 folding_start_structure_width, folding_start_structure_weight,
-                 repeats_min_repeats, repeats_repeat_length, repeats_weight,
-                 fasta):
+def parse_options():
+    parser = argparse.ArgumentParser(
+        prog='vaxpress',
+        description='VaxPress: A Codon Optimizer for mRNA Vaccine Design')
 
-    inputseq = next(SeqIO.parse(fasta, 'fasta'))
+    grp = parser.add_argument_group('Input/Output Options')
+    grp.add_argument('-o', '--output', required=True, help='output file')
+    grp.add_argument('-q', '--quiet', default=False, action='store_true', help='do not print progress')
+    grp.add_argument('--print-top', type=int, default=10, help='print top and bottom N sequences (default: 10)')
+
+    grp = parser.add_argument_group('Execution Options')
+    grp.add_argument('--seed', type=int, default=922, help='random seed (default: 922)')
+    grp.add_argument('-p', '--processes', type=int, default=4, help='number of processes to use (default: 4)')
+
+    grp = parser.add_argument_group('Sequence Options')
+    grp.add_argument('--codon-table', default='standard', help='codon table (default: standard)')
+    grp.add_argument('--random-initialization', default=False, action='store_true', help='randomize all codons at the beginning')
+
+    grp = parser.add_argument_group('Optimization Options')
+    grp.add_argument('--iterations', type=int, default=50, help='number of iterations (default: 50)')
+    grp.add_argument('--offsprings', type=int, default=100, help='number of offsprings per iteration (default: 100)')
+    grp.add_argument('--survivors', type=int, default=3, help='number of survivors per iteration (default: 3)')
+    grp.add_argument('--initial-mutation-rate', type=float, default=0.1, help='initial mutation rate (default: 0.1)')
+    grp.add_argument('--winddown-trigger', type=int, default=15, help='number of iterations with the same best score to trigger mutation stabilization (default: 15)')
+    grp.add_argument('--winddown-rate', type=float, default=0.9, help='mutation rate multiplier when mutation stabilization is triggered (default: 0.9)')
+
+    grp = parser.add_argument_group('Fitness - iCodon')
+    grp.add_argument('--icodon-species', default='human', help='iCodon RNA stability optimizer: species (default: human)')
+    grp.add_argument('--icodon-weight', type=float, default=1.0, help='scoring weight for iCodon predicted stability (default: 1.0)')
+
+    grp = parser.add_argument_group('Fitness - Uridines')
+    grp.add_argument('--ucount-weight', type=float, default=3.0, help='scoring weight for U count minimizer (default: 3.0)')
+
+    grp = parser.add_argument_group('Fitness - GC Ratio')
+    grp.add_argument('--gc-window-size', type=int, default=50, help='size of window for GC content calculation (default: 50)')
+    grp.add_argument('--gc-stride', type=int, default=5, help='size of stride for GC content calculation (default: 5)')
+    grp.add_argument('--gc-weight', type=int, default=3, help='GC penalty score (default: 3)')
+
+    grp = parser.add_argument_group('Fitness - RNA Folding')
+    grp.add_argument('--folding-off', default=False, action='store_true', help='disable secondary structure folding')
+    grp.add_argument('--folding-engine', default='vienna', choices=['vienna', 'linearfold'],
+                     help='RNA folding engine: vienna (default) or linearfold')
+    grp.add_argument('--folding-mfe-weight', type=float, default=1.0, help='scoring weight for MFE (default: 1.0)')
+    grp.add_argument('--folding-start-structure-width', type=int, default=15, help='width in nt of unfolded region near the start codon (default: 15)')
+    grp.add_argument('--folding-start-structure-weight', type=int, default=1, help='penalty weight for folded start codon region (default: 1)')
+
+    grp = parser.add_argument_group('Fitness - Tandem Repeats')
+    grp.add_argument('--repeats-min-repeats', type=int, default=2, help='minimum number of repeats to be considered as a tandem repeat (default: 2)')
+    grp.add_argument('--repeats-repeat-length', type=int, default=10, help='minimum length of repeats to be considered as a tandem repeat (default: 10)')
+    grp.add_argument('--repeats-weight', type=float, default=1.0, help='scoring weight for tandem repeats (default: 1.0)')
+
+    parser.add_argument('fasta', help='input fasta file containing the CDS sequence')
+
+    return parser.parse_args()
+
+def run_vaxpress():
+    args = parse_options()
+
+    inputseq = next(SeqIO.parse(args.fasta, 'fasta'))
     seqdescr = inputseq.description
     cdsseq = str(inputseq.seq).replace('T', 'U')
     if len(cdsseq) % 3 != 0:
@@ -83,36 +96,36 @@ def run_vaxpress(output, seed, processes, quiet, print_top, codon_table, random_
         raise ValueError(f'Invalid letters in CDS sequence: {",".join(invalid_letters)}')
 
     scoring_options = ScoringOptions(
-        iCodon_species=icodon_species,
-        iCodon_weight=icodon_weight,
-        ucount_weight=ucount_weight,
-        gc_window_size=gc_window_size,
-        gc_stride=gc_stride,
-        gc_weight=gc_weight,
-        folding_off=folding_off,
-        folding_engine=folding_engine,
-        folding_mfe_weight=folding_mfe_weight,
-        folding_start_structure_width=folding_start_structure_width,
-        folding_start_structure_weight=folding_start_structure_weight,
-        repeats_min_repeats=repeats_min_repeats,
-        repeats_repeat_length=repeats_repeat_length,
-        repeats_weight=repeats_weight,
+        iCodon_species=args.icodon_species,
+        iCodon_weight=args.icodon_weight,
+        ucount_weight=args.ucount_weight,
+        gc_window_size=args.gc_window_size,
+        gc_stride=args.gc_stride,
+        gc_weight=args.gc_weight,
+        folding_off=args.folding_off,
+        folding_engine=args.folding_engine,
+        folding_mfe_weight=args.folding_mfe_weight,
+        folding_start_structure_width=args.folding_start_structure_width,
+        folding_start_structure_weight=args.folding_start_structure_weight,
+        repeats_min_repeats=args.repeats_min_repeats,
+        repeats_repeat_length=args.repeats_repeat_length,
+        repeats_weight=args.repeats_weight,
     )
 
     iteration_options = IterationOptions(
-        n_iterations=iterations,
-        n_offsprings=offsprings,
-        n_survivors=survivors,
-        initial_mutation_rate=initial_mutation_rate,
-        winddown_trigger=winddown_trigger,
-        winddown_rate=winddown_rate,
+        n_iterations=args.iterations,
+        n_offsprings=args.offsprings,
+        n_survivors=args.survivors,
+        initial_mutation_rate=args.initial_mutation_rate,
+        winddown_trigger=args.winddown_trigger,
+        winddown_rate=args.winddown_rate,
     )
 
     evochamber = CDSEvolutionChamber(
-        cdsseq, output, scoring_options, iteration_options,
-        seed=seed, processes=processes,
-        codon_table=codon_table, quiet=quiet, seq_description=seqdescr,
-        print_top_mutants=print_top, random_initialization=random_initialization)
+        cdsseq, args.output, scoring_options, iteration_options,
+        seed=args.seed, processes=args.processes,
+        codon_table=args.codon_table, quiet=args.quiet, seq_description=seqdescr,
+        print_top_mutants=args.print_top, random_initialization=args.random_initialization)
 
     evochamber.run()
 
