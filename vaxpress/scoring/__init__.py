ChangLabSNU/VaxPress

commit f374cf81b85956e0c3378c6957777d59dd873926

Mark ScoringFunction ABC to enforce to implement `score`

vaxpress/scoring/__init__.py
@@ -23,7 +23,9 @@
 # THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-class ScoringFunction:
+import abc
+
+class ScoringFunction(abc.ABC):
 
     name = 'noname'
     description = 'no description'
@@ -59,6 +61,7 @@ class ScoringFunction:
         except KeyboardInterrupt:
             return None
 
+    @abc.abstractmethod
     def score(self, seqs):
         raise NotImplementedError
 

commit 4225ec1839337863566581d1f1d4f64f779eb8f5

Fix scoring function search broken by f374cf8

vaxpress/scoring/__init__.py
@@ -77,7 +77,7 @@ def discover_scoring_functions(addon_paths):
     def scan_module(mod):
         for objname in dir(mod):
             obj = getattr(mod, objname)
-            if (obj is not ScoringFunction and type(obj) == type and
+            if (obj is not ScoringFunction and type(obj) == abc.ABCMeta and
                     issubclass(obj, ScoringFunction)):
                 funcs[obj.name] = obj
 