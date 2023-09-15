1c0e1ad1121c63beb927715b2a49a3251ec48982
Hyeshik Chang <hyeshik@snu.ac.kr>
2023-09-15 22:59:26 +0900

    Change the copyright holder.

--- vaxpress/evolution_chamber.py
+++ vaxpress/evolution_chamber.py
@@ -1,7 +1,7 @@
 #
 # VaxPress
 #
-# Copyright 2023 Hyeshik Chang
+# Copyright 2023 Seoul National University
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
